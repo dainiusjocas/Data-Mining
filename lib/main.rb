2c10c1a2c86a6ab994114f5f0038ce989363fcec
unknown <Dainius@.(none)>
2010-11-30 22:11:44 +0100

    local

--- lib/main.rb
+++ lib/main.rb
@@ -1,8 +1,10 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
+require 'dataset.rb'
 
 puts "Sveikas pasauli!"
-
+data = Dataset.new
+data.dosmth
 # Dainiau, nesamoninga karves funkcija....
 #
 #(0..10).each do
