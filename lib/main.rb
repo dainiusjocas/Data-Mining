2d8acaa7018a185e626347642fafa4138578a70a
Dainius Jocas <dainiusjocas@gmail.com>
2010-11-30 21:48:23 +0100

    local shit

--- lib/main.rb
+++ lib/main.rb
@@ -1,10 +1,24 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
 
-puts "Sveikas pasauli!"
 
-# Dainiau, nesamoninga karves funkcija....
-#
-#(0..10).each do
-#  puts "moo"
-#end
+class Dataset
+  i=0
+  tuple[]
+
+  def read_file path
+    file = File.new("#{path}", "r")
+  end
+
+  def build_dataset
+    while (line = file.gets)
+      i++
+      if !line.start_with?("@")
+       tuple[i] = line.to_s
+      end
+    end
+  end
+end
+
+
+Dataset.read_file "auto_mpg.arff"
+Dataset.build_dataset
+
