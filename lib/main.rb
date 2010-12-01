ac23811f75980f509ac3ce205e84720849054eb0
Dainius Jocas <dainiusjocas@gmail.com>
2010-12-01 22:46:50 +0100

    sorting dataset to numeric and nominal attributes

--- lib/main.rb
+++ lib/main.rb
@@ -1,11 +1,27 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
-require 'dataset'
+require 'distance'
 
-dataset = Dataset.new
-dataset.build_dataset "../test/adult.arff"
+distance = Distance.new
 
-dataset.get_attribute(5, 1)
+distance.fetch_tuples(0, 1)
+@num_value_index = distance.get_num_index
+@nom_value_index = distance.get_nom_index
+@att_names = distance.dataset_inst.att_names
 
 
+puts ""
+puts "Numeric value index and name"
+@num_value_index.each do |item|
+  print item 
+  print " "
+  puts @att_names.values_at(item)
+end
 
+puts ""
+puts "Nominal value index and name"
+@nom_value_index.each do |item|
+  print item
+  print " "
+  puts @att_names.values_at(item)
+end
